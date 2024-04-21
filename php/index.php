1fcf5d75dca0f227397a9e2fe1d6a99c5fa5ac32
Quamagi <149438605+Quamagi@users.noreply.github.com>
2024-04-21 08:15:18 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><?php echo "Hola Mundo!" ?></H2>
+    <p>Si funciona pero eso no es suficiente</p>
+</body>
+</html>
