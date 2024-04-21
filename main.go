cbf6c25fd0c26dfa35c5f78cd2660403be267d6d
Quamagi <149438605+Quamagi@users.noreply.github.com>
2024-04-21 10:42:09 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os/exec"
@@ -18,6 +19,14 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Configurar CORS
+	addCORSHeaders(w, r)
+
+	// Manejar solicitudes de opciones CORS
+	if r.Method == http.MethodOptions {
+		return
+	}
+
 	// Definir la ruta base del directorio de PHP
 	baseDir := "C:\\golang\\server\\php"
 
@@ -33,9 +42,28 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	serveScript(w, r, scriptPath)
 }
 
-func serveScript(w http.ResponseWriter, _ *http.Request, scriptPath string) {
+func addCORSHeaders(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func serveScript(w http.ResponseWriter, r *http.Request, scriptPath string) {
 	// Ejecutar el script PHP
-	out, err := exec.Command("C:\\php\\php-cgi.exe", "-f", scriptPath).Output()
+	cmd := exec.Command("C:\\php\\php-cgi.exe", "-f", scriptPath)
+
+	// Pasar el cuerpo de la solicitud al script PHP para métodos POST, PUT y DELETE
+	if r.Method == http.MethodPost || r.Method == http.MethodPut || r.Method == http.MethodDelete {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Error al leer el cuerpo de la solicitud: %v", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		cmd.Stdin = &Pipe{bytes: body}
+	}
+
+	out, err := cmd.Output()
 	if err != nil {
 		log.Printf("Error al ejecutar el script PHP: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -47,4 +75,20 @@ func serveScript(w http.ResponseWriter, _ *http.Request, scriptPath string) {
 
 	// Imprimir la salida del script PHP para depuración
 	log.Printf("Salida del script PHP: %s", out)
+}
+
+// Pipe es un tipo auxiliar que implementa la interfaz io.Reader
+type Pipe struct {
+	bytes []byte
+	pos   int
+}
+
+// Read implementa el método Read de io.Reader
+func (p *Pipe) Read(b []byte) (int, error) {
+	n := copy(b, p.bytes[p.pos:])
+	p.pos += n
+	if n == 0 {
+		return 0, io.EOF
+	}
+	return n, nil
 }
